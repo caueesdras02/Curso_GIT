0f8b7c776665a7d67c4793f56ad6b555c922ffae
caueesdras02 <caueesdras02@gmail.com>
2025-05-13 20:42:05 -0300

    alteração

--- codigo.py
+++ codigo.py
@@ -1,3 +1,7 @@
-n1 = 5
-n2 = 8
-print(f"A soma dos número é {n1 + n2}")
+n1 = int(input("Insira o 1° número desejado: "))
+n2 = int(input("Insira o 2° número desejado: "))
+n3 = n1 + n2
+if n3 <= 10:
+    print("A soma dos número é menor que 10!")
+else:
+    print("A soma dos número é maior que 10!")
